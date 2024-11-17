ab93e5f9cb6a6aa3b677be6e7d8312f54a66fab5
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-11-17 11:44:14 +1100

    scaffold ui/ux for common components and layout

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 90])
+
+<img src="https://picsum.photos/seed/{{ random_int(0, 1000) }}/{{ $width }}" alt="Employer Logo" class="rounded-xl">
---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Employer Name</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Job Title</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
