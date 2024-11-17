27ba05483a2c55206eb55955961b611daf4caade
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-11-17 12:48:05 +1100

    set up tags migration and factories for employer/job/tag, set up unit tests using PEST to verify rudimentary job logic.

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => fake()->jobTitle(),
+            'salary' => fake()->randomElement(['$50,000 AUD','$70,000 AUD','$80,000 AUD','$90,000 AUD','$110,000 AUD', '$120,000 AUD', '$150,000 AUD']),
+            'location' => 'Remote',
+            'schedule' => 'Full Time',
+            'url' => fake()->url(),
+            'featured' => false
         ];
     }
 }
