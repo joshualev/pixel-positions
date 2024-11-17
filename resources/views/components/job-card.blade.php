6287259c7f7e86d44bd3066fa8e225f623562eb5
Joshua Levinson <124539621+jlevinson-intermedia@users.noreply.github.com>
2024-11-17 13:23:26 +1100

    set up seeders, and start integration for dynamic data display for jobs and assosciated data with seeded information.

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,18 @@
+@props(['job'])
+
 <x-panel class="flex-col">
-    <div class="self-start text-sm">Employer Name</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8 text-center space-y-6">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Job Title</h3>
-        <p class="text-sm my-4">Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm my-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-end mt-auto">
         <div>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Devops</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag size="small"/>
+            @endforeach
         </div>
 
         <x-employer-logo :width="42" />
